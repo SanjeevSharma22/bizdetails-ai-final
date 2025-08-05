430e537d871fa549d108a58adac7baa0740f1a26
SanjeevSharma22 <sanjusanjeev22@gmail.com>
2025-08-06 04:56:35 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,6 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
-const root = createRoot(document.getElementById("root"));
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const root = createRoot(document.getElementById('root'));
- main
-root.render(<App />);
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
